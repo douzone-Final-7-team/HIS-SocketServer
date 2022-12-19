fcae0e10cdd0cd5a17bc2d7a698b2fe7d21163e4
choko5901 <ehdgk5901@gmail.com>
2022-12-19 13:32:45 +0900

    test node

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         //origin: "http://192.168.0.183:3000", // 서버와 통신할 클라인언트 주소
-        origin: "http://192.168.0.195:3000",
+        origin: "http://localhost:3000",
         methods:["GET","POST"]
     }
 })
